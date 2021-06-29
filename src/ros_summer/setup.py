20c5acb4f6ad1546e7e632ff611e4c0cbc0c4840
Faithfulness Alamu <vaguemail369@gmail.com>
2021-06-29 11:56:26 +0100

    feat: add a launch file

--- src/ros_summer/setup.py
+++ src/ros_summer/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = "ros_summer"
@@ -9,6 +11,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name), glob("launch/*.xml")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
