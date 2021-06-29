cd48407f11c69a1cf34b38c8e2e055e776a661c5
Faithfulness Alamu <vaguemail369@gmail.com>
2021-06-29 08:45:59 +0100

    feat(counter): implement counting to stdout

--- src/ros_summer/ros_summer/counter.py
+++ src/ros_summer/ros_summer/counter.py
@@ -1,5 +1,23 @@
+import rclpy
+from rclpy import Node
+
+
+class CountingNode(Node):
+    def __init__(self):
+        super().__init__("counting_node")
+        self._logger = self.get_logger()
+        self._count = 0
+        self.create_timer(1.0, self.count_callback)
+
+    def count_callback(self):
+        self._logger.info(self._count)
+        self._count += 1
+
+
 def main():
-    print("Counting")
+    rclpy.init()
+    rclpy.spin(CountingNode())
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
