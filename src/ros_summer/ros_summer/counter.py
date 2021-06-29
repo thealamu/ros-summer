9acca69f5c18220d910befb86045a6557e7cb72b
Faithfulness Alamu <vaguemail369@gmail.com>
2021-06-29 09:08:32 +0100

    feat(counter): publish counts to a topic

--- src/ros_summer/ros_summer/counter.py
+++ src/ros_summer/ros_summer/counter.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Int64
 
 
 class CountingNode(Node):
@@ -7,10 +8,16 @@ class CountingNode(Node):
         super().__init__("counting_node")
         self._logger = self.get_logger()
         self._count = 0
+        self._publisher = self.create_publisher(Int64, "count", 10)
         self.create_timer(1.0, self.count_callback)
 
     def count_callback(self):
         self._logger.info(f"Counter: {self._count}")
+
+        dt = Int64()
+        dt.data = self._count
+        self._publisher.publish(dt)
+
         self._count += 1
 
 
