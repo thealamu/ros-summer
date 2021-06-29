3bb814fdd3fe4be465b4c8f4ab40fa4855c57bf7
Faithfulness Alamu <vaguemail369@gmail.com>
2021-06-29 09:20:48 +0100

    feat(summer): implement the summer node

--- src/ros_summer/ros_summer/summer.py
+++ src/ros_summer/ros_summer/summer.py
@@ -1,5 +1,24 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int64
+
+
+class SummerNode(Node):
+    def __init__(self):
+        super().__init__("summer_node")
+        self._logger = self.get_logger()
+        self._sum = 0
+        self.create_subscription(Int64, "count", self.summer_callback, 10)
+
+    def summer_callback(self, msg):
+        self._sum += msg.data
+        self._logger.info(f"Total: {self._sum}")
+
+
 def main():
-    pass
+    rclpy.init()
+    rclpy.spin(SummerNode())
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
